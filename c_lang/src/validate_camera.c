360c638fcb11f1a477e9b17a5eca8a868545c5d1
Kodai Hanadate <khanadat@K.local>
2025-12-17 13:56:17 +0900

    fix: validate_sphere.c

--- c_lang/src/validate_camera.c
+++ c_lang/src/validate_camera.c
@@ -2,39 +2,50 @@
 #include "libft.h"
 #include <stdlib.h>
 
+static void	err_camera(void)
+{
+	static char	*msg = \
+"\x1b[31m""SET CAMARA'S INFORMATION CORRECTLY.\n" \
+"\x1b[0m""C [Coordinate]\t[Unit Direction Vector]\t[Vertical FOV(0-180)]\n" \
+"\x1b[0m""C 0,0,0\t\t0,-1,0\t\t\t70";
+
+	ft_putendl_fd(msg, STDERR_FILENO);
+}
+
 // You need to imprement new function
 // which return true if two doubles are almost equal
 // but in the first place, libft doesn't
 // have fabs nor strtof so...
-int	validate_camera(char *line_ptr)
+int	validate_camera(char *line)
 {
 	double	d_tmp;
 	size_t	i;
 
-	line_ptr++;
 	i = 0;
 	while (i++ < 3)
 	{
-		d_tmp = ft_strtod(line_ptr, &line_ptr);
-		if (i != 3 && *(line_ptr++) != ',')
-			return (EXIT_FAILURE);
+		d_tmp = ft_strtod(line, &line);
+		if (i != 3 && *(line++) != ',')
+			return (err_camera(), EXIT_FAILURE);
 	}
-	if (skip_spaces(&line_ptr))
-		return (EXIT_FAILURE);
+	if (skip_spaces(&line))
+		return (err_camera(), EXIT_FAILURE);
 	i = 0;
 	d_tmp = 0;
 	while (i++ < 3)
 	{
-		d_tmp += pow2(ft_strtod(line_ptr, &line_ptr));
-		if (i != 3 && *(line_ptr++) != ',')
-			return (EXIT_FAILURE);
+		d_tmp += pow2(ft_strtod(line, &line));
+		if (i != 3 && *(line++) != ',')
+			return (err_camera(), EXIT_FAILURE);
 	}
 	if (0.001 < d_tmp - 1)
-		return (EXIT_FAILURE);
-	if (skip_spaces(&line_ptr))
-		return (EXIT_FAILURE);
-	d_tmp = ft_strtod(line_ptr, &line_ptr);
+		return (err_camera(), EXIT_FAILURE);
+	if (skip_spaces(&line))
+		return (err_camera(), EXIT_FAILURE);
+	d_tmp = ft_strtod(line, &line);
 	if (d_tmp < 0.0f || 180.0f < d_tmp)
-		return (EXIT_FAILURE);
-	return (EXIT_SUCCESS);
+		return (err_camera(), EXIT_FAILURE);
+	if (*line == '\n' || *line == '\0')
+		return (EXIT_SUCCESS);
+	return (err_camera(), EXIT_SUCCESS);
 }
